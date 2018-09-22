d05b8031c7282cd3b46b366dece3051565864f72
huabrandon0 <huabrandon0@gmail.com>
2018-09-22 02:33:21 -0700

    ad on restart

---
+++ Assets/Scripts/Advertisements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace TWM.Advertisements
+{
+    public class Advertisements : MonoBehaviour
+    {
+        public void ShowVideoAd()
+        {
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show("video", new ShowOptions() { resultCallback = HandleShowResult });
+            }
+        }
+
+        void HandleShowResult(ShowResult result)
+        {
+            switch (result)
+            {
+                case ShowResult.Finished:
+                    Debug.Log("ad finished :)");
+                    break;
+                case ShowResult.Skipped:
+                    Debug.Log("ad skipped :(");
+                    break;
+                case ShowResult.Failed:
+                    Debug.Log("ad failed :O");
+                    break;
+            }
+        }
+    }
+}

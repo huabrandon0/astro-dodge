ca2a98814ed8aa4e633c16bbc2a95933ee532b57
huabrandon0 <huabrandon0@gmail.com>
2018-09-27 22:59:10 -0700

    uhhhh

--- Assets/GPGSIds.cs
+++ Assets/GPGSIds.cs
@@ -24,7 +24,7 @@
 
 public static class GPGSIds
 {
-        public const string leaderboard_leaderboard = "CgkI0PGIse0cEAIQAQ"; // <GPGSID>
+        public const string leaderboard_leaderboard = "CgkIvPKL_egSEAIQAQ"; // <GPGSID>
 
 }
 
